4e434294d3e83185d7d1375729aceddd3f01d1b1
Matias Maiorano <m.maiorano@itsrizzoli.it>
2022-05-06 13:52:15 +0200

    miglioramento

--- mongodb/utente.py
+++ mongodb/utente.py
@@ -4,9 +4,118 @@
 
 from pymongo import MongoClient
 import datetime
+import os
 
-if __name__ != '__main__':
-    raise Exception("Lanciami come programma principale grazie")
+
+def print_info_acquisto(e):
+    while True:
+        clear_console()
+        print_evento(e)
+        i = 1
+        scelte = []
+        menu = ""
+        for x in e["posti"].keys():
+            print(
+                f'[{i}] - {x}: {e["posti"][x]["prezzo"]}EUR, {e["posti"][x]["posti"]} posti rimanenti')
+            scelte.append(x)
+            i += 1
+        print("[9] - Indietro")
+        decisione = input("> ")
+        try:
+            decisione = int(decisione) - 1
+            lst_scelte = [x for x in range(i-1)]
+            lst_scelte.append(9-1) # uscita
+            if decisione in lst_scelte:
+                break
+            else:
+                print("Input invalido")
+                input("Premi un tasto per continuare\n>")
+        except:
+            print("Input invalido")
+            input("Premi un tasto per continuare\n>")
+        if decisione == 9 - 1:
+            return
+    while True:
+        try:
+            clear_console()
+            print_evento(e)
+            quantita = int(input("\nInserisci numero biglietti da acquistare: "))
+            break
+        except:
+            print("Input invalido")
+            input("Premi un tasto per continuare\n>")
+            clear_console()
+            continue
+    acquista(e, scelte[decisione], quantita)
+    pass
+
+def clear_console():
+    os.system("cls")
+    pass
+
+
+def utente():
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # TODO inserimento data "massima"
+
+    index = 0
+
+    result = []
+    while True:
+        if not result:
+            result_exist = False
+        while result_exist == False:
+            req = input("Inserisci nome artista: ")
+            query = {
+                "$and": [
+                    {"cantanti": {"$in": [req]}},
+                    {"dataora": {"$gt": now}},
+                    {"posti_totali": {"$gt": 0}}
+                    # TODO luogo range < 7 km
+                ]
+            }
+            result = list(mongo.eventi.concerti.find(filter=query))
+            if result:
+                result_exist = True
+            else:
+                print("Nessun risultato per il seguente artitsta:", req)
+        clear_console()
+        print(f"Risultati per {req} | pagina numero [{index + 1}/{len(result)}]")
+        print_evento(result[index])
+        print("[1] - Precedente")
+        print("[2] - Successivo")
+        print("[3] - Seleziona evento")
+        print("[9] - Esci")
+        scelta = input("> ")
+        try:
+            scelta = int(scelta)
+        except:
+            scelta = 10 # metto 10 se non posso fare il casting della scelta
+            clear_console()
+            print("Input invalido")
+            input("Premi un tasto per continuare\n>")
+        if scelta == 1 and index != 0:
+            index -= 1
+            clear_console()
+        elif scelta == 2:
+            if index == len(result) - 1:
+                clear_console()
+                print(f"Eventi finiti per l'artitsta {req}")
+                input("Premi un tasto per continuare\n>")
+                index = -1
+            index += 1
+            clear_console()
+        elif scelta == 3:
+            print_info_acquisto(result[index])
+        elif scelta == 9:
+            clear_console()
+            break
+        else:
+            print("Input invalido")
+            input("Premi un tasto per continuare\n>")
+
+
+    pass
 
 
 def print_evento(e):
@@ -31,7 +140,7 @@ def acquista(e, tipo_posto, quantity=1, interrupt_after_purchase=True):
             'posti_totali': e["posti_totali"] - quantity,
             f'posti.{tipo_posto}.posti': e["posti"][tipo_posto]["posti"] - quantity
         }}
-                                     )
+    )
 
     # Genero biglietto
     for name in names:
@@ -52,65 +161,11 @@ def acquista(e, tipo_posto, quantity=1, interrupt_after_purchase=True):
         exit()
 
 
-def print_info_acquisto(e):
-    i = 1
-    scelte = []
-    for x in e["posti"].keys():
-        print(f'[{i}] - {x}: {e["posti"][x]["prezzo"]}EUR, {e["posti"][x]["posti"]} posti rimanenti')
-        scelte.append(x)
-        i += 1
-    print("[9] - Indietro")
-    decisione = int(input("> ")) - 1
-    if decisione == 9 - 1:
-        return
-    while True:
-        try:
-            quantita = int(input("Inserisci numero biglietti da acquistare: "))
-            break
-        except e:
-            print("Inserisci un numero")
-            continue
-    acquista(e, scelte[decisione], quantita)
-
-
-mongo = MongoClient('mongodb://localhost:37000/')
-now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-req = input("Inserisci nome artista: ")
-# TODO inserimento data "massima"
-
-query = {
-    "$and": [
-        {"cantanti": {"$in": [req]}},
-        {"dataora": {"$gt": now}},
-        {"posti_totali": {"$gt": 0}}
-        # TODO luogo range < 7 km
-    ]
-}
-
-index = 0
-
-while True:
-    result = list(mongo.eventi.concerti.find(filter=query))
-    if not result:
-        # TODO richiere l'input
-        raise Exception("Artista non presente")
-    print_evento(result[index])
-    print("[1] - Precedente")
-    print("[2] - Successivo")
-    print("[3] - Seleziona evento")
-    print("[9] - Esci")
-    scelta = int(input("> "))
-    if scelta == 1 and index != 0:
-        index -= 1
-    elif scelta == 2 and index != len(result) - 1:
-        index += 1
-    elif scelta == 3:
-        print_info_acquisto(result[index])
-    elif scelta == 9:
-        break
-    else:
-        print("Invalido")
-
-mongo.close()
-print("Grazie per usato il servizio di biglietteria")
+if __name__ != '__main__':
+    raise Exception("Lanciami come programma principale grazie")
+else:
+    clear_console()
+    mongo = MongoClient('mongodb://localhost:37000/')
+    utente()
+    mongo.close()
+    print("Grazie per usato il servizio di biglietteria")
