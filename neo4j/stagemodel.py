ae7d47b2e43200e763444b43ba146645b35644dc
MatteoSchifano <matvinsch@gmail.com>
2022-06-10 11:16:21 +0200

    da visionare con il prof

--- neo4j/stagemodel.py
+++ neo4j/stagemodel.py
@@ -24,32 +24,37 @@ class Neo4jModel:
         """
         diz = json.dumps(parms)
         with self.driver.session() as session:
-            id_nodo = session.run(f"CREATE (n:$label {diz}) RETURN id(n)", label=label)
+            # non vanno entrambe le righe sottostanti 
+            id_nodo = session.run(f"CREATE (n:{label}" + "{nome:'luca'}) RETURN id(n)", label=label)
+            
+            id_nodo = session.run(f"CREATE (n:{label} " + diz + ") RETURN id(n)", label=label)
+            id_nodo = session.run("CREATE (n:$label " + diz + ") RETURN id(n)", label=label)
 
         return id_nodo
 
-    def create_link(self, origin_id, destination_id, label):
+    def create_link(self, origin_id, destination_id, label, **parms):
+        diz = json.dumps(parms)
         with self.driver.session() as session:
-            session.run("""MATCH (a),(b)
+            session.run(f"""MATCH (a),(b)
                         WHERE id(a) = $origin_id and id(b) = $destination_id
-                        CREATE (a)-[r:$label]->(b)""", 
+                        CREATE (a)-[r:$label {diz}]->(b)""", 
                         origin_id=origin_id, 
                         label=label, 
                         destination_id=destination_id)
    
     def return_node(self, node_id=None, label=None):
         # TODO prevedere la return per parametri dei nodi 
-        assert node_id is not None and label is None
-        assert node_id is None and label is not None
         
         if label:
             with self.driver.session() as session:
-                return session.run("""MATCH (n: $label) RETURN n""")
+                return session.run(f"""MATCH (n: {label}) RETURN n""")
         
         with self.driver.session() as session:
             return session.run("""MATCH (n)
                         WHERE id(n) = $node_id
                         RETURN n""", node_id=node_id)
+        
+        return None
 
     def return_label(self, node_id):
         with self.driver.session() as session:
@@ -60,3 +65,6 @@ class Neo4jModel:
     def empty_database(self):
         with self.driver.session() as session:
             return session.run("match (n) detach delete n return count(n)").single()[0]
+
+if __name__ == "__main__":
+    print(Neo4jModel("bolt://localhost:7687").return_node(label= 'rifugio'))
